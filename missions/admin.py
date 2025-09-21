5f89490f97794a77ef00b53ac7e32ac0bb90c85b
junh5ng <wnsghd1130@dgu.ac.kr>
2025-09-22 05:49:24 +0900

    Mission, UserMission 관리자 설정

--- missions/admin.py
+++ missions/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Mission, UserMission
+
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ('id','content','is_active','created_at')
+    list_filter = ('is_active',)
+    search_fields = ('content',)
+
+@admin.register(UserMission)
+class UserMissionAdmin(admin.ModelAdmin):
+    list_display = ('id','user','mission','completed','given_up','scheduled_at','created_at')
+    list_filter = ('completed','given_up','mood')
+    search_fields = ('user__username','mission__content','memo')
