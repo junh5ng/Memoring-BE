9bea67be2b98d2e7e0de12d94dfd5555e9169913
junh5ng <wnsghd1130@dgu.ac.kr>
2025-09-23 19:51:41 +0900

    UserMissionCurrentSerializer에 status 필드 추가

--- missions/serializers.py
+++ missions/serializers.py
@@ -9,16 +9,23 @@ class MissionSerializer(serializers.ModelSerializer):
 
 
 class UserMissionCurrentSerializer(serializers.ModelSerializer):
+    
+    # 제목/설명은 Mission의 content를 재사용
     title = serializers.CharField(source='mission.content', read_only=True)
     description = serializers.CharField(source='mission.content', read_only=True)  # 프로토타입: content 재사용
+    
+    # 모델 속성 그대로 노출
     voice_uploaded = serializers.SerializerMethodField()
     listenable = serializers.SerializerMethodField()
+    # status 추가
+    status = serializers.CharField(source="status", read_only=True)
 
     class Meta:
         model = UserMission
         fields = [
             'id','title','description','scheduled_at','alarm_offset_minutes',
-            'voice_uploaded','completed','listenable'
+            'voice_uploaded','completed','listenable',
+            'status', # 추가
         ]
 
     def get_voice_uploaded(self, obj): return bool(obj.voice)
